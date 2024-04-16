a69374dbd26093d6810a076b60ed41eeec6d79fc
arihantp2 <ap.machinelearning@gmail.com>
2024-04-17 01:04:32 +0530

    Secondary movies container added

---
+++ src/hooks/usePopularMovies.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { API_options } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addPopularMovies } from "../utils/movieSlice";
+
+const usePopularMovies = () => {
+  const dispatch = useDispatch();
+
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?page=1",
+      API_options
+    );
+
+    const json = await data.json();
+    console.log(json.results);
+    dispatch(addPopularMovies(json.results));
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+};
+
+export default usePopularMovies;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,10 +7,13 @@ export const USER_AVTAR =
 export const BACKGROUND_IMG =
   "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
-  export const API_options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2N2ZkN2M0NmY2ZTNkOTJjNzMwYTZiMjBiYTViMzQyNiIsInN1YiI6IjY2MTUwODFiOTgyZjc0MDE3ZTYxMWU2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.p_D8XzHXzjEkg4jVaElRAyvMDUQUU_3G7hw5sd4QIGI'
-    }
-  };
+export const API_options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2N2ZkN2M0NmY2ZTNkOTJjNzMwYTZiMjBiYTViMzQyNiIsInN1YiI6IjY2MTUwODFiOTgyZjc0MDE3ZTYxMWU2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.p_D8XzHXzjEkg4jVaElRAyvMDUQUU_3G7hw5sd4QIGI",
+  },
+};
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w780";
