f740cd8f23a719f761f2c4a8e5ebd93b9fac888b
arihantp2 <ap.machinelearning@gmail.com>
2024-04-21 15:29:16 +0530

    API Calls handled

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,10 +1,12 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addTrailerVideo } from "../utils/movieSlice";
 import { API_options } from "../utils/constants";
 import { useEffect } from "react";
 
 const useMovieTrailer = (movieId) => {
   const dispatch = useDispatch();
+
+  const trailerVideo = useSelector((store) => store.movie.trailerVideo);
 
   // fetch trailer video & updating the store with trailer video data
   const getMovieVideos = async () => {
@@ -30,7 +32,7 @@ const useMovieTrailer = (movieId) => {
   };
 
   useEffect(() => {
-    getMovieVideos();
+    !trailerVideo && getMovieVideos();
   }, []);
 };
 
